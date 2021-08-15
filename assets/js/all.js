a69b38793069856f2ea16ebefd8e5685c886f6fa
elaine <elaine7062002@gmail.com>
2021-08-15 12:58:49 +0800

    Update 2021-08-15T04:58:46.899Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,7 +5,17 @@ $(function () {
 }); // swiper
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
